89301aa9fb5f95fa0447ba23aeadbc59260c43fe
Agent9777 <shouryadeep05@outlook.com>
2025-04-21 19:53:13 +0530

    added dependencies

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/auth", authRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
