af1b7c11dbcc5c0b4997da6992041dbc17246dab
Agent9777 <shouryadeep05@outlook.com>
2025-04-21 15:55:53 +0530

    updated connection

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "localhost",  // MySQL server host
-  user: "root",       // MySQL username
-  password: "Sssddd1@2345",  // Correct password for MySQL
-  database: "fdb",  // Your database name
+  host: "sql12.freesqldatabase.com",
+  user: "sql12774451",
+  password: "5j4rIBVf3n",
+  database: "sql12774451",
+  port: 3306
 });
 
 connection.connect((err) => {
@@ -12,7 +13,7 @@ connection.connect((err) => {
     console.error("Error connecting to the database:", err);
     return;
   }
-  console.log("Connected to the MySQL database");
+  console.log("Connected to the FreeSQLDatabase successfully");
 });
 
-module.exports = connection;  // Export the connection
+module.exports = connection;
